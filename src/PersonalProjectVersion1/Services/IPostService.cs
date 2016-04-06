0300f1f95b9f61a08c2bc860f3ae6adc80353202
Cory Couty <syncrose@gmail.com>
2016-04-06 16:38:37 -0700

    Added Post service + interface

---
+++ src/PersonalProjectVersion1/Services/IPostService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using PersonalProjectVersion1.Models;
+
+namespace PersonalProjectVersion1.Services
+{
+    public interface IPostService
+    {
+        void AddPost(Post post);
+        Post getPost(int Id);
+        List<Post> GetPosts();
+        void UpdatePost(Post post);
+    }
+}
---
+++ src/PersonalProjectVersion1/Services/PostService.cs
@@ -0,0 +1,40 @@
+﻿using PersonalProjectVersion1.Models;
+using PersonalProjectVersion1.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalProjectVersion1.Services
+{
+    public class PostService : IPostService
+    {
+        IGenericRepository _repo;
+
+        public PostService(IGenericRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public List<Post> GetPosts()
+        {
+            var data = _repo.Query<Post>().ToList();
+            return data;
+        }
+
+        public Post getPost(int Id)
+        {
+            var data = _repo.Query<Post>().Where(p => p.Id == Id).Include(p => p.LinkedMessages).FirstOrDefault();
+            return data;
+        }
+
+        public void AddPost(Post post)
+        {
+            _repo.Add(post);
+        }
+        public void UpdatePost(Post post)
+        {
+            _repo.Update(post);
+        }
+    }
+}
